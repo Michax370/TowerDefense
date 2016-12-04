d49c5412a6983d3f4425f87e349cf6d647c3938c
benjamin <benjamin.michau@sogeti.com>
2016-12-04 15:56:06 +0100

    First Commit
    Class creation

---
+++ TowerDefense/Map.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class Map
+    {
+        private List<Tower> Towers;
+        private List<Mob> Mobs;
+
+        public Map()
+        {
+
+        }
+    }
+}
